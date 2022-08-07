94011ca949bdbb9e26d588a5e3aec831c48a1d54
Ortal Wikoff <owikoff@gmail.com>
2022-08-07 17:45:04 -0400

    updated connection.js

--- config/connection.js
+++ config/connection.js
@@ -17,12 +17,3 @@ if (process.env.JAWSDB_URL) {
     }
   );
 }
-
-// module.exports = sequelize;
-
-// const Sequelize = require('sequelize');
-// require('dotenv').config();
-
-// const sequelize = new Sequelize(process.env.MYSQLURI)
-
-// module.exports = sequelize;
